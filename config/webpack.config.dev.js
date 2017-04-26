c805c646c333c2eb05487ea9b715ac5a65151c1c
Jari Zwarts <jarizw@gmail.com>
2017-04-26 02:20:56 +0200

    Fix hot reload from extension (use custom webpack dev client)

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -41,8 +41,9 @@ module.exports = {
     // to bring better experience for Create React App users. You can replace
     // the line below with these two lines if you prefer the stock client:
     // require.resolve('webpack-dev-server/client') + '?/',
-    require.resolve('webpack/hot/dev-server'),
+    // require.resolve('webpack/hot/dev-server'),
     // require.resolve('react-dev-utils/webpackHotDevClient'),
+    require.resolve('./webpackHotDevClient'),
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
     // Finally, this is your app's code:
