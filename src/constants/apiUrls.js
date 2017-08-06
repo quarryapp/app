e0240e9fb47622e3d74a947d3182f5a6474457d0
Jeff Reiner <theundeadghost@gmail.com>
2017-08-06 09:27:40 -0400

    :sparkles: Changing BASE_URL to hosted backend

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:3131/'; // todo add production/dev check
+const BASE_URL = 'http://quarry.host/'; // todo add production/dev check
 
 export const feedUrl = (page = 1) => `${BASE_URL}feed?page=${page}`;
